46e30a6967ef323f4a95aee446ab91922e33a2a6
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-03 13:13:48 -0800

    Implemented initial state and set state

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,7 +1,21 @@
 var React = require('react');
+var AppActions = require('../action/AppActions');
+var AppStore = require('../stores/AppStore');
 var SearchForm = require('./SearchForm.js');
 
 var App = React.createClass({
+  getInitialState: function() {
+    return getAppState();
+  },
+
+  componentDidMount: function() {
+    AppStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount: function() {
+    AppStore.removeChangeListener(this._onChange);
+  },
+
   render: function() {
     return (
       <div>
