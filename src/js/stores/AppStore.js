315f3cd8a025ccc20dac2df4ecacb4744ee844d1
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-03 13:59:21 -0800

    Implemented action when movie is retrieved and searched

--- src/js/stores/AppStore.js
+++ src/js/stores/AppStore.js
@@ -2,6 +2,7 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
+var AppAPI = require('../utils/appAPI.js');
 
 var CHANGE_EVENT = 'change';
 
@@ -10,6 +11,15 @@ var _movies = [];
 var _selected = '';
 
 var AppStore = assign({}, EventEmitter.prototype, {
+
+  //Once the movies is found, store them in the array
+  setMovieResults: function(movies) {
+    _movies = movies;
+  },
+
+  getMovieResults: function() {
+    return _movies;
+  },
 
   emitChange: function() {
     this.emit(CHANGE_EVENT);
@@ -22,5 +32,26 @@ var AppStore = assign({}, EventEmitter.prototype, {
   }
 });
 
+AppDispatcher.register(function(payload) {
+  var action = payload.action;
+
+  switch(action.actionType) {
+    case AppConstants.SEARCH_MOVIES:
+        //action.movie.title came from AppActions.js
+        //Need to put action before the everything else
+        console.log('searching for movie ' + action.movie.title);
+        AppAPI.searchMovies(action.movie);
+        AppStore.emit(CHANGE_EVENT);
+        break;
+
+    case AppConstants.RECEIVE_MOVIE_RESULTS:
+        //Calling the above function
+        AppStore.setMovieResults(action.movies);
+        AppStore.emit(CHANGE_EVENT);
+        break;
+  }
+
+  return true;
+});
 
 module.exports = AppStore;
