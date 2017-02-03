6f0e7a04f5bf50e2719dfe8345255bb8eee531c2
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-03 13:52:43 -0800

    Implemented function to interact with API

--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -1,1 +1,18 @@
 var AppActions = require('../action/AppActions');
+
+module.exports = {
+  searchMovies: function(movie) {
+    $.ajax({
+      //movie.title came from SearchForm component as an input
+      url: 'http://www.omdbapi.com/?s=' + movie.title,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        AppActions.receiveMovieResults(data.Search);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        alert(err);
+      }.bind(this)
+    });
+  }
+}
