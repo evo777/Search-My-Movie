259e41ac716b01bbd6429c933e17a1e5f12cd588
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-02 17:39:57 -0800

    Added store.js file

---
+++ src/js/stores/AppStore.js
@@ -0,0 +1,4 @@
+var AppDispatcher = require('../dispatcher/AppDispatcher');
+var AppConstants = require('../constants/AppConstants');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
