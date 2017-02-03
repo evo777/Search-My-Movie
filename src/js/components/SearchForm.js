35bf67232e15c6bc7065ba82fbe511f9023c16af
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-03 01:12:52 -0800

    Implemented search bar and search button feature

--- src/js/components/SearchForm.js
+++ src/js/components/SearchForm.js
@@ -1,5 +1,19 @@
 var React = require('react');
 
-var SearchForm = React.createClass({});
+var SearchForm = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1 className="text-center">Search A Movie</h1>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <input type="text" className="form-control" ref="title" placeholder="Please enter a movie title" />
+          </div>
+            <button className="btn btn-primary btn-block">Search Movies</button>
+        </form>
+      </div>
+    )
+  }
+});
 
 module.exports = SearchForm;
