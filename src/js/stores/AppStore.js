cc33525d739f0063400ce2b44c4fc39436b60234
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-02 17:43:11 -0800

    Added methods that will be used to retrieve movies

--- src/js/stores/AppStore.js
+++ src/js/stores/AppStore.js
@@ -2,3 +2,23 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
+
+//Store movies in this array
+var _movies = [];
+var _selected = '';
+
+var AppStore = assign({}, EventEmitter.prototype, {
+
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  },
+  addChangeListener: function(callback) {
+    this.on('change', callback);
+  },
+  removeChangeListener: function(callback) {
+    this.removeListener('change', callback);
+  }
+});
+
+
+module.exports = AppStore;
