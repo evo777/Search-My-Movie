d726e01bf87b9b80c3cb7022395241dde725bf33
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-03 13:34:08 -0800

    Implemented MovieResults component

--- src/js/components/MovieResults.js
+++ src/js/components/MovieResults.js
@@ -1,3 +1,22 @@
 var React = require('react');
 var AppActions = require('../action/AppActions.js');
 var AppStore = require('../stores/AppStore');
+
+var MovieResults = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h3 className="text-center">Results</h3>
+        {
+          this.props.movies.map(function(movie, i) {
+            return (
+              <Movie movie={movie} key={i} />
+            )
+          })
+        }
+      </div>
+    )
+  }
+});
+
+module.exports = MovieResults;
