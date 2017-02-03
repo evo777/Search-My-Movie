1c26623f127d7c64cb0f9026e698d006943ac681
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-02-02 17:50:02 -0800

    Change "Hi" to SearchForm component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,10 +1,11 @@
 var React = require('react');
+var SearchForm = require('./SearchForm.js');
 
 var App = React.createClass({
   render: function() {
     return (
       <div>
-        Hi
+        <SearchForm />
       </div>
     )
   }
